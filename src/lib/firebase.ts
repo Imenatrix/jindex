72beccbd0db94d69b7effab197ba379f7b7bd907
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-06-08 10:46:43 -0300

    Adiciona conexão com o firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAuc81GuOIUvSDQGS-l6zirZFyGWlIdym0",
+  authDomain: "streaming-cli-001.firebaseapp.com",
+  projectId: "streaming-cli-001",
+  storageBucket: "streaming-cli-001.appspot.com",
+  messagingSenderId: "744014383620",
+  appId: "1:744014383620:web:8cd1a68c2cbf0e97dc8351",
+  measurementId: "G-92B05RE634"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
