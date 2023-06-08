7a5fa15b685d57d50f520f0b717753b9154d6c00
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-06-08 13:51:21 -0300

    Implementa criação de streaming

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,13 +1,31 @@
+import { LOCATION, PROJECT } from '$env/static/private'
 import '$lib/firebase'
-import { addDoc, collection, getFirestore } from 'firebase/firestore'
+import { createChannel, createInput, getInput, startChannel } from '$lib/livestream.js'
+import { addDoc, collection, getFirestore, updateDoc } from 'firebase/firestore'
 
 export const actions = {
     create : async ({ request }) => {
         const data = await request.formData()
         const db = getFirestore()
         const cameras = collection(db, 'cameras')
-        addDoc(cameras, {
-            name : data.get('name')
+
+        const name = data.get('name')
+        const inputId = (name as string) + '-input'
+        const channelId = (name as string) + '-channel'
+        const outputUrl = 'gs://' + PROJECT + '/' + (name as string) + '_output'
+
+        const doc = await addDoc(cameras, {
+            name : name,
+            status : 'CREATING'
+        })
+        await createInput(PROJECT, LOCATION, inputId)
+        await createChannel(PROJECT, LOCATION, inputId, channelId, outputUrl)
+        await startChannel(PROJECT, LOCATION, channelId)
+        const input = await getInput(PROJECT, LOCATION, inputId)
+
+        updateDoc(doc, {
+            status : 'DONE',
+            uri : input.uri
         })
     }
 }
