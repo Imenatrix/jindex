aeb8497efb13100dfe39326ca6dfb4d7de9d24b3
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-06-08 14:34:14 -0300

    Melhora UI do cadastro de cameras

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
-<form method="post" action='?/create'>
-    <label>
-        Nome:
-        <input type="text" name="name">
-    </label>
-    <button>Create</button>
-</form>
+<div class="flex justify-center items-center h-screen w-screen">
+    <div class="card w-96 bg-base-200">
+        <form method="post" action='?/create' class="card-body">
+            <input type="text" name="name" placeholder="Nome..." class="input">
+            <button class="btn btn-primary">Create</button>
+        </form>
+    </div>
+</div>
