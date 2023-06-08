ca6a63cfe000a47471751a2bf1a6bb583f6ce854
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-06-08 17:51:46 -0300

    Adiciona carregamento na UI de criação de cameras

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,25 @@
+<script lang='ts'>
+    import { enhance } from "$app/forms"
+    let creating = false
+</script>
+
 <div class="flex justify-center items-center h-screen w-screen">
     <div class="card w-96 bg-base-200">
-        <form method="post" action='?/create' class="card-body">
+        <form method="post" action='?/create' class="card-body" use:enhance={() => {
+            creating = true
+            return async ({ update }) => {
+                creating = false
+                await update()
+            }
+        }}>
             <input type="text" name="name" placeholder="Nome..." class="input">
-            <button class="btn btn-primary">Create</button>
+            <button class="btn btn-primary">
+                {#if creating}
+                    <div class="loading loading-spinner"></div>
+                {:else}
+                    Create
+                {/if}
+            </button>
         </form>
     </div>
 </div>
