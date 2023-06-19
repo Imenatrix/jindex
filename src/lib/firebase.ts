195a4b9880ac53ece2488fef921cb81b7456c7dc
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-06-19 17:39:04 -0300

    Muda projeto firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,13 +6,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAuc81GuOIUvSDQGS-l6zirZFyGWlIdym0",
-  authDomain: "streaming-cli-001.firebaseapp.com",
-  projectId: "streaming-cli-001",
-  storageBucket: "streaming-cli-001.appspot.com",
-  messagingSenderId: "744014383620",
-  appId: "1:744014383620:web:8cd1a68c2cbf0e97dc8351",
-  measurementId: "G-92B05RE634"
+  apiKey: "AIzaSyARBKzaouW4fSbBwlJrVzCDYLIUZnku_ds",
+  authDomain: "surfview-001.firebaseapp.com",
+  projectId: "surfview-001",
+  storageBucket: "surfview-001.appspot.com",
+  messagingSenderId: "778677178141",
+  appId: "1:778677178141:web:17f15426c5929c1da31bf4",
+  measurementId: "G-V2S1CF6BP3"
 };
 
 // Initialize Firebase
