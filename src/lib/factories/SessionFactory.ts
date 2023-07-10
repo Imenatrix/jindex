7cbdcd68f844b6ceded616298519bc5d99cfe1ee
Ruan Gomes <ruan@flowih.com>
2023-07-10 18:32:02 -0300

    Implementa SessionFactory usando Cloud Storage

--- src/lib/factories/SessionFactory.ts
+++ src/lib/factories/SessionFactory.ts
@@ -1,19 +1,32 @@
 import Session from "$lib/entities/Session"
-import type { DocumentData, QueryDocumentSnapshot, SnapshotOptions } from "firebase/firestore"
-import fs from 'fs'
+import { getFirestore, type DocumentData, type QueryDocumentSnapshot, type SnapshotOptions, collection, getDoc, doc } from "firebase/firestore"
+import { Storage } from '@google-cloud/storage'
+import { env } from '$env/dynamic/private'
+import '$lib/firebase'
+import { CameraFactory } from "./CameraFactory"
+import { toKebabCase } from "$lib/utils/strings"
 
 export class SessionFactory {
     static converter = {
         toFirestore(camera : Session): DocumentData {
             return {...camera}
         },
-        fromFirestore(
+        async fromFirestore(
             snapshot: QueryDocumentSnapshot,
             options: SnapshotOptions
-        ): Session {
+        ): Promise<Session> {
+            const storage = new Storage()
             const data = snapshot.data(options)
-            // TODO: Implement using GCloud
-            const filenames = fs.readdirSync('output').filter(filename => filename.endsWith('.ts'))
+
+            const db = getFirestore()
+            const cameras = collection(db, 'cameras')
+            const camera = (await getDoc(doc(cameras, data.camera_id).withConverter(CameraFactory.converter))).data()
+
+            const slug = toKebabCase(camera?.name ?? '')
+            const outputName = slug + '-output'
+
+            const [files] = await storage.bucket(env.PROJECT ?? '').getFiles({prefix : outputName + '/'})
+            const filenames = files.map(file => file.name).filter(file => file.endsWith('.ts'))
             const time = data.time.toDate()
             time.setMilliseconds(0)
             return new Session(snapshot.id, time, filenames)
