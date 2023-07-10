7f5e83118efc74171cb9ce7c5ea8676c2134fbf9
Ruan Gomes <ruan@flowih.com>
2023-07-10 17:15:46 -0300

    Implementa endpoint basico de download

---
+++ src/routes/download/+page.server.ts
@@ -0,0 +1,43 @@
+import fs from 'fs'
+import ffmpeg from 'fluent-ffmpeg'
+import '$lib/firebase'
+import { collection, getFirestore, getDocs } from 'firebase/firestore'
+import Downloader from '$lib/services/Downloader'
+import { SessionFactory } from '$lib/factories/SessionFactory.js'
+
+const SEGMENT_LENGTH = 2
+
+export async function load() {
+    const db = getFirestore()
+    const cameras = collection(db, 'cameras')
+    const data = (await getDocs(cameras)).docs.map(doc => ({
+        id : doc.id,
+        name : doc.data().name,
+    }))
+    return {
+        cameras : data
+    }
+}
+
+export const actions = {
+    default : async ({ request }) => {
+        const data = await request.formData()
+        const id = data.get('id') as string
+        const t0 = new Date(data.get('t0') as string)
+        const t1 = new Date(data.get('t1') as string)
+
+        const db = getFirestore()
+
+        const sessions = (await getDocs(collection(db, 'sessions').withConverter(SessionFactory.converter))).docs.map(doc => doc.data())
+        const downloader = new Downloader(SEGMENT_LENGTH, sessions)
+        const filenames = downloader.getSegmentsBetween(t0, t1)
+        makeVideo(filenames)
+    }
+}
+
+function makeVideo(files : string[]) {
+    const videos = files.map(file => fs.readFileSync('output/' + file))
+    const video = Buffer.concat(videos)
+    fs.writeFileSync('video.ts', video)
+    ffmpeg().input('video.ts').output('video.mp4').on('end', () => console.log('Finished')).run()
+}
